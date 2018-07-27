a3aa1b59f8fa34cc129a5efd7b03f234b3c83293
foxsidark <foxsidark@yandex.ru>
2018-07-27 18:42:31 +0300

    NewControler
    Обновлен контролер сделано получше

---
+++ Assets/script/MoveControlTRUE.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MoveControlTRUE : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+  //  [SerializeField] private Text _Text;
+public void OnDrag(PointerEventData eventData)
+{
+        /* gameObject.GetComponent<RectTransform>().anchoredPosition=
+             new Vector2( 
+                 eventData.pointerCurrentRaycast.screenPosition.x, 
+             eventData.pointerCurrentRaycast.screenPosition.y);*/
+        gameObject.transform.position = eventData.pointerCurrentRaycast.worldPosition;
+       // Debug.Log(eventData.pointerCurrentRaycast.screenPosition.x + " "+ eventData.pressPosition.y);
+}
+
+public void OnBeginDrag(PointerEventData eventData)
+{
+        // _Text.text = "You dragging!";
+        //Debug.Log("You dragging!");
+}
+
+public void OnEndDrag(PointerEventData eventData)
+{
+        //_Text.text = "Drag me!";
+        gameObject.transform.localPosition = new Vector3();
+       // Debug.Log("You dragging!");
+    }
+
+	
+   
+}
--- Assets/script/NewControler.cs
+++ Assets/script/NewControler.cs
@@ -24,42 +24,11 @@ public class NewControler : MonoBehaviour {
     
     bool flag = false;
    
-    Touch touch;
-	void FixedUpdate () {
+    
+	void Update () {
         
 
-        for (int i = 0; i < Input.touchCount; i++)
-        {
-          // if (TouchPhase.Began == Input.touches[i].phase)
-            {
-                RaycastHit2D hit;
-                Vector3 P = camera.ScreenToWorldPoint(Input.touches[i].position);
-                Vector2 P2d = new Vector2(P.x, P.y);
-                hit = Physics2D.Raycast(P2d, P2d);
-                if (hit)
-                {
-                    if (hit.collider.gameObject.tag == "ControlMove")
-                    {
-                        flag = true;
-                        touch = Input.touches[i];
-
-                    }
-                }
-
-            }
-        }
-        if (Input.touchCount>0 && flag && touch.phase == TouchPhase.Ended)
-        {
-            flag = false;
-            Control.transform.localPosition = new Vector3();
-
-        }
-        if (flag)
-        {
-            Vector3 P = camera.ScreenToWorldPoint(touch.position);
-            Vector2 P2d = new Vector2(P.x, P.y);
-            Control.transform.position = P2d;
-        }
+        
         float XY = Mathf.Sqrt(Control.transform.localPosition.y* Control.transform.localPosition.y+ Control.transform.localPosition.x* Control.transform.localPosition.x);
         float X = 0;
         float Y = 0;
